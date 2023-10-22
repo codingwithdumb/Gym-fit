a7c9ab02eddd7911c2a5d3e8530963496652c092
Deepak Sharma <deepaksharma1942003@gmail.com>
2023-10-22 20:56:30 +0530

    editable

--- fit-sexy/src/component/BodyLanguage.js
+++ fit-sexy/src/component/BodyLanguage.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import Nav from '../common/Nav'
 import Footer from '../common/Footer'
-
+import Beopen from '../img/bodyLanguage/openbe.jpg'
+import voicebe from '../img/bodyLanguage/voicebe.jpg'
+import eyebe from '../img/bodyLanguage/eyebe.jpg'
+import demonbe from '../img/bodyLanguage/demonbe.jpg'
+import smilebe from '../img/bodyLanguage/smilebe.jpg'
+import Bepresent from '../img/bodyLanguage/presentbe.jpg'
 
 const BodyLanguage = () => {
   return (
@@ -14,8 +19,8 @@ const BodyLanguage = () => {
         <div className="container">
  <div className="row">
           <div className="col-md-4">
-<div className="card" style={{width: '18rem'}}>
-  <img src="https://www.quiet-corner.com/wp-content/uploads/2016/06/4t4t4t.jpg" className="card-img-top" alt="..." />
+<div className="card fixHeightmin" style={{width: '18rem'}}>
+  <img src={Beopen} className="card-img-top" alt="..." />
   <div className="card-body">
     <h5 className="card-title">Be Open</h5>
     <p className="card-text">As a personal trainer, your body language should always be open.</p>
@@ -24,8 +29,8 @@ const BodyLanguage = () => {
 </div>
           </div>
           <div className="col-md-4">
-          <div className="card" style={{width: '18rem'}}>
-  <img src="..." className="card-img-top" alt="..." />
+          <div className="card fixHeightmin" style={{width: '18rem'}}>
+  <img src={Bepresent} className="card-img-top" alt="..." />
   <div className="card-body">
     <h5 className="card-title">Always Be Present </h5>
     <p className="card-text">Being fully present and focused on your client is a crucial part of having good body language for personal trainers. </p>
@@ -34,8 +39,8 @@ const BodyLanguage = () => {
 </div>
           </div>
           <div className="col-md-4">
-          <div className="card" style={{width: '18rem'}}>
-  <img src="..." className="card-img-top" alt="..." />
+          <div className="card fixHeightmin" style={{width: '18rem'}}>
+  <img src={voicebe} className="card-img-top" alt="..." />
   <div className="card-body">
     <h5 className="card-title">Project Your Voice</h5>
     <p className="card-text">As well as your physical movements and positions, an equally important element of body language is the voice.</p>
@@ -46,8 +51,8 @@ const BodyLanguage = () => {
         </div>
         <div className="row mt-5">
             <div className="col-md-4">
-          <div className="card" style={{width: '18rem'}}>
-  <img src="..." className="card-img-top" alt="..." />
+          <div className="card fixHeightmin" style={{width: '18rem'}}>
+  <img src={eyebe} className="card-img-top" alt="..." />
   <div className="card-body">
     <h5 className="card-title">Make Eye Contact With</h5>
     <p className="card-text">Eye contact is important in almost all interactions, so is an essential part of personal trainer body language.</p>
@@ -56,8 +61,8 @@ const BodyLanguage = () => {
 </div>
      </div>
             <div className="col-md-4">
-            <div className="card" style={{width: '18rem'}}>
-  <img src="..." className="card-img-top" alt="..." />
+            <div className="card fixHeightmin" style={{width: '18rem'}}>
+  <img src={demonbe} className="card-img-top" alt="..." />
   <div className="card-body">
     <h5 className="card-title">Demonstrate Exercises</h5>
     <p className="card-text">Another good way to demonstrate good body language as a personal trainer is to always perform demonstrations.</p>
@@ -66,8 +71,8 @@ const BodyLanguage = () => {
 </div>
  </div>
             <div className="col-md-4">
-            <div className="card" style={{width: '18rem'}}>
-  <img src="..." className="card-img-top" alt="..." />
+            <div className="card fixHeightmin" style={{width: '18rem'}}>
+  <img src={smilebe} className="card-img-top" alt="..." />
   <div className="card-body">
     <h5 className="card-title">Smile!</h5>
     <p className="card-text">It may sound obvious, but smiling is an often overlooked aspect of having good body language as a personal trainer.</p>
@@ -76,38 +81,7 @@ const BodyLanguage = () => {
 </div>
     </div>
         </div>
-        <div className="row mt-5">
-            <div className="col-md-4">
-          <div className="card" style={{width: '18rem'}}>
-  <img src="..." className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">Dress Appropriately</h5>
-    <p className="card-text">You may not have thought it, but dressing appropriately is an important part of having good personal trainer body language. </p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-     </div>
-            <div className="col-md-4">
-            <div className="card" style={{width: '18rem'}}>
-  <img src="..." className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
- </div>
-            <div className="col-md-4">
-            <div className="card" style={{width: '18rem'}}>
-  <img src="..." className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    </div>
-        </div>
+      
         </div>
        
       </div>
